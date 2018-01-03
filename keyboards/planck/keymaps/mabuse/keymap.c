32e791575513d4a37c4f51bd65e2a6c1a9a35572
Manfred Berndtgen <mberndtgen@gmail.com>
2018-01-03 12:51:56 +0100

    added mabuse layout

---
+++ keyboards/planck/keymaps/mabuse/keymap.c
@@ -0,0 +1,154 @@
+#include "planck.h"
+#include "keymap.h"
+#include "keymap_german.h"
+#include "action_layer.h"
+
+extern keymap_config_t keymap_config;
+
+// Each layer gets a name for readability, which is then used in the keymap matrix below.
+// The underscores don't mean anything - you can have a layer called STUFF or any other name.
+// Layer names don't all need to be of the same length, obviously, and you can also skip them
+// entirely and just use numbers.
+#define _QWERTY 0
+#define _LOWER 1
+#define _RAISE 2
+#define _FUNC 3
+#define _ADJUST 16
+
+enum planck_keycodes {
+  QWERTY = SAFE_RANGE,
+  FUNC,
+  LOWER,
+  RAISE
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+/* Qwerty
+ * ,-----------------------------------------------------------------------------------.
+ * | Tab  |   Q  |   W  |   E  |   R  |   T  |   Z  |   U  |   I  |   O  |   P  | Bksp |
+ * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * | Esc  |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |  ;:  |  '"  |
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * | Shft |   Y  |   X  |   C  |   V  |   B  |   N  |   M  |  ,<  |  .>  |  /?  | Enter|
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | Fn   | Ctrl | Alt  | GUI  |Lower |    Space    |Raise | Down |Right | PgDn |  Del|
+ * `-----------------------------------------------------------------------------------'
+ */
+[_QWERTY] = {
+  {KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,  KC_T,   KC_Z,   KC_U,  KC_I,    KC_O,     KC_P,    KC_BSPC              },
+  {KC_ESC,    KC_A,    KC_S,    KC_D,    KC_F,  KC_G,   KC_H,   KC_J,  KC_K,    KC_L,     KC_SCLN, KC_QUOT              },
+  {KC_LSFT,   KC_Y,    KC_X,    KC_C,    KC_V,  KC_B,   KC_N,   KC_M,  KC_COMM, KC_DOT,   KC_SLSH, MT(MOD_LSFT, KC_ENT) },
+  {MO(_FUNC), KC_LCTL, KC_LALT, KC_LGUI, LOWER, KC_SPC, KC_SPC, RAISE, KC_DOWN, KC_RIGHT, KC_PGDN, KC_DEL               } 
+},
+
+/* Function
+ * ,-----------------------------------------------------------------------------------.
+ * |      | EUR  |      |      |      |      |      |  ue  |      |  oe  |      |      |
+ * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * |      |  ae  |  ss  |      |      |      |      |      |      |      |      |      |
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * | Shft |      |      |      |      |      |      |      |      |      |      |      |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      |      |      |             |      |      |      |      |      |
+ * `-----------------------------------------------------------------------------------'
+ */
+[_FUNC] = {
+  {_______, DE_EURO, _______, _______, _______, _______, _______, DE_UE,   _______, DE_OE,   _______, _______ },
+  {_______, DE_AE,   DE_SS,   _______, _______, _______, _______, _______, _______, _______, _______, _______ },
+  {KC_LSFT, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ },
+  {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ }
+},
+
+/* Lower
+ * ,-----------------------------------------------------------------------------------.
+ * |  `   |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  | Del  |
+ * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * |  Del |      |      |      |      |      |      |   _  |   =  |   {  |   }  |      |
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * | Shift|      |      |      |      |      |      |      |      |      |   |  |      |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      |      |      |    Space    |      | Pos1 | End  | Home | End  |
+ * `-----------------------------------------------------------------------------------'
+ */
+[_LOWER] = {
+    {KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR,    KC_LPRN,    KC_RPRN, KC_DEL  }, 
+    {KC_DEL,  _______, _______, _______, _______, _______, _______, KC_UNDS, KC_EQL,     KC_LCBR,    KC_RCBR, _______ },
+    {KC_LSFT, _______, _______, _______, _______, _______, _______, _______, _______,    _______,    KC_PIPE, _______ },
+    {_______, _______, _______, _______, _______, KC_SPC,  KC_SPC,  _______, LCTL(KC_A), LCTL(KC_E), KC_HOME, KC_END }
+},
+
+/* Raise
+ * ,-----------------------------------------------------------------------------------.
+ * |  ~   |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |  0   |      |
+ * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * |      |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   -  |   +  |   [  |  ]   |   '  |
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * |      |  F7  |  F8  |  F9  |  F10 | F11  | F12  |      |   <  |   >  |  \   |      |
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * |      |      |      |      |      |    Space    |      |  Up  | Left | PgUp |      |
+ * `-----------------------------------------------------------------------------------'
+ */
+[_RAISE] = {
+    {KC_TILD, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,   KC_6,   KC_7,    KC_8,    KC_9,    KC_0,      _______ },
+    {_______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,  KC_F6,  KC_MINS, KC_PLUS, KC_LBRC, KC_RBRC,   KC_QUOT },
+    {_______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11, KC_F12, _______, KC_LT,   KC_GT,   KC_BSLASH, _______ },
+    {_______, _______, _______, _______, _______, KC_SPC, KC_SPC, _______, KC_UP,   KC_LEFT, KC_PGUP,   _______ }
+},
+    
+
+/* Adjust (Lower + Raise)
+ * ,-----------------------------------------------------------------------------------.
+ * | ???? | Reset|Qwerty|      |      |      |      |      |      |      |      |  Del |
+ * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * | CAPS |      |      |      |      |      |      | Mute | Vol+ | Play |      |      |
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * |      |      |      |      |      |      |      | Prev | Vol- | Next |      |      |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      |      |      |             |      |      |      |      |      |
+ * `-----------------------------------------------------------------------------------'
+ */
+[_ADJUST] = {
+  {M(0),    RESET,   QWERTY,  _______, _______, _______, _______, _______,             _______,           _______,              _______,  KC_DEL  },
+  {KC_CAPS, _______, _______, _______, _______, _______, _______, KC_AUDIO_MUTE,       KC_AUDIO_VOL_UP,   KC_MEDIA_PLAY_PAUSE,  _______,  _______ },
+  {_______, _______, _______, _______, _______, _______, _______, KC_MEDIA_PREV_TRACK, KC_AUDIO_VOL_DOWN, KC_MEDIA_NEXT_TRACK,  _______,  _______ },
+  {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ }
+},
+
+};
+
+
+void persistant_default_layer_set(uint16_t default_layer) {
+  eeconfig_update_default_layer(default_layer);
+  default_layer_set(default_layer);
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+   switch (keycode) {
+    case QWERTY:
+      if (record->event.pressed) {
+        persistant_default_layer_set(1UL<<_QWERTY);
+      }
+      return false;
+    case LOWER:
+      if (record->event.pressed) {
+        layer_on(_LOWER);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      } else {
+        layer_off(_LOWER);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      }
+      return false;
+    case RAISE:
+      if (record->event.pressed) {
+        layer_on(_RAISE);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      } else {
+        layer_off(_RAISE);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      }
+      return false;
+  }
+  return true;
+}
+
---
+++ keyboards/planck/keymaps/mabuse/readme.md
@@ -0,0 +1,79 @@
+# mabuse Custom Planck Layout
+# forked from AJP10304 with little alterations
+
+**Note:** In the tables below where there are two characters on a key, 
+the second is the output when shift is applied.
+  
+**Note:** The below tables assume a UK layout.
+
+**Note:** If you a bit older then you probly think of the Commodore 64 when recognizing only two cursor keys (the others, Up and Left are shifted by Raise). This is intentional. There's no need for four cursor keys.
+
+##### Main Qwerty Layer 
+
+* Tab: when held, operates as shift.
+* Enter: when held, operates as shift.
+* MENU: perform right-click
+
+|      |        |       |       |       |       |       |       |       |       |       |       |
+| ---- | :----: | ----: | ----: | ----: | ----: | ----: | ----: | ----: | ----: | ----: | ----: |
+| Esc  | Q      | W     | E     | R     | T     | Z     | U     | I     | O     | P     | Bksp  |
+| Tab  | A      | S     | D     | F     | G     | H     | J     | K     | L     | ;:    | '"    |
+| Shft | Y      | X     | C     | V     | B     | N     | M     | ,<    | .>    | /?    | Enter |
+| Fn   | Ctrl   | Alt   | OS    | Lower | Space | Raise | Down  | Right | PgDn  | Del   |       |
+
+##### Function Layer
+Activated when `fn` held in the above `qwerty` layer.
+
+|       |        |       |       |       |       |       |       |       |       |       |       |
+| ----  | :----: | ----: | ----: | ----: | ----: | ----: | ----: | ----: | ----: | ----: | ----: |
+|       | EUR    |       |       |       |       |       | ue    |       | oe    |       |       |
+|       | ae     | ss    |       |       |       |       |       |       |       |       |       |
+| Shift |        |       |       |       |       |       |       |       |       |       |       |
+|       |        |       |       |       |       |       |       |       |       |       |       |
+
+##### Lower Layer
+Activated when `Lower` is held in the above `qwerty` layer.
+
+* Numbers are along the top row, their shifted counterparts are on row 2.
+* WrdBks: `backspace` with `ctrl` applied. I.e. delete a word.  
+* WrdDel: `delete` with `ctrl` applied. I.e. forward delete a word.
+
+|       |        |       |       |       |       |       |       |       |       |       |       |
+| ----  | :----: | ----: | ----: | ----: | ----: | ----: | ----: | ----: | ----: | ----: | ----: |
+| `     | !      | @     | #     | $     | %     | ^     | &     | *     | (     | )     | Del   |
+| Del   |        |       |       |       |       |       | _     | =     | {     | }     |       |
+| Shift |        |       |       |       |       |       |       |       |       |       |       |
+|       |        |       |       |       | Space |       | Pos1  | End   | Home  | End   |       |
+ 
+ ##### Raise Layer
+ Activated when `Raise` is held in the above `qwerty` layer.
+ 
+ * Preferred layer for typing brackets.
+ * Allows for cursor navigation to be used solely with the right hand.
+ * WRDSEL: Select the word where the cursor is.
+ * |< and >|: Apply `ctrl` to `left` and `right` respectively for word 
+
+|      |        |       |       |       |       |       |       |       |       |       |         |
+| ---- | :----: | ----: | ----: | ----: | ----: | ----: | ----: | ----: | ----: | ----: | -----:  |
+| ~    | 1      | 2     | 3     | 4     | 5     | 6     | 7     | 8     | 9     | 0     |         |
+|      | F1     | F2    | F3    | F4    | F5    | F6    | -     | +     | [     | ]     | '       |
+|      | F7     | F8    | F9    | F10   | F11   | F12   |       | <     | >     | \     |         |
+|      |        |       |       |       | Space |       | Up    | Left  | PgUp  |       |         |
+|      |        |       |       |       |       |       |       |       |       |       |         |
+ 
+##### Lower + Raise
+Activated when `Lower` and `Raise` are held together the above `qwerty` layer.
+
+* Audio controls in the same position as cursor keys from the `Raise` layer.  
+* ????: Runs a macro for outputting sensitive information.  Do not use this store passwords.
+* Reset: Enter bootloader for flashing firmware to the keyboard.
+* CAPS: Toggle caps lock.
+* DYN: Enter `DYN` layer.
+
+|      |        |        |       |       |       |       |       |       |       |       |         |
+| ---- | :----: | ----:  | ----: | ----: | ----: | ----: | ----: | ----: | ----: | ----: | -----:  |
+| ???? | Reset  | Qwerty |       |       |       |       |       |       |       |       | Del     |
+| CAPS |        |        |       |       |       |       | Mute  | Vol+  | Play  |       |         |
+|      |        |        |       |       |       |       | Prev  | Vol-  | Next  |       |         |
+|      |        |        |       |       |       |       |       |       |       |       |         |
+ 
